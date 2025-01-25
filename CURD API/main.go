eace6f5907d7410341d708c99725140ee5885ec9
shahariaz <shahariaz.info@gmail.com>
2025-01-26 01:08:04 +0600

    finished project

--- CURD API/main.go
+++ CURD API/main.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -35,6 +38,7 @@ func deleteMovie(w http.ResponseWriter, r *http.Request){
 			 break
 		}
 	}
+	json.NewEncoder(w).Encode(movies)
 	
 }
 func getMovie(w http.ResponseWriter, r *http.Request){
@@ -43,11 +47,36 @@ func getMovie(w http.ResponseWriter, r *http.Request){
 	for  index,item := range movies{
 		if item.ID == params["id"]{
 			json.NewEncoder(w).Encode(movies[index])
-			break
+			return
+		}
+	}
+}
+func createMovie(w http.ResponseWriter, r *http.Request ){
+	w.Header().Set("content-type","application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies,movie)
+	json.NewEncoder(w).Encode(movie)
+
+}
+func updateMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("content-type","application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"]{
+			movies= append(movies[:index],movies[index+1:]...)
+			var movie Movie
+		_ = json.NewDecoder(r.Body).Decode(&movie)
+		movie.ID = params["id"]
+		movies = append(movies,movie)
+		json.NewEncoder(w).Encode(movie)
+		return
 		}
 	}
 }
 func main(){
+	rand.Seed(time.Now().UnixNano())
 	r := mux.NewRouter()
 	movies = append(movies, Movie{ID: "1", Isbn: "448743", Title: "Movie One", Director: &Director{FirstName: "John", LastName: "Doe"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "847564", Title: "Movie Two", Director: &Director{FirstName: "Steve", LastName: "Smith"}})
